11f56d63d502b0d16bb7d3088cc40ab386572a32
Suraj Singh <yousurajsingh@gmail.com>
2025-03-27 01:13:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your code here
+const images = document.querySelectorAll("img");
+
+images.forEach(img => {
+    img.addEventListener("dragstart", dragStart);
+    img.addEventListener("dragover", dragOver);
+    img.addEventListener("drop", drop);
+    img.addEventListener("dragenter", preventDefault);
+    img.addEventListener("dragleave", preventDefault);
+});
+
+let dragSource = null;
+
+function dragStart(e) {
+    dragSource = this;
+    e.dataTransfer.setData("text/plain", this.id);
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function drop(e) {
+    e.preventDefault();
+    const dragTarget = this;
+
+    if (dragSource && dragSource !== dragTarget) {
+        // Swap images
+        const sourceParent = dragSource.parentNode;
+        const targetParent = dragTarget.parentNode;
+
+        targetParent.replaceChild(dragSource, dragTarget);
+        sourceParent.appendChild(dragTarget);
+    }
+}
+
+function preventDefault(e) {
+    e.preventDefault();
+}
